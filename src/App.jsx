d374adffbd3c65a50090127e3e2d2ed4db40232e
David Aghassi <3680126+Aghassi@users.noreply.github.com>
2019-11-30 11:39:59 -0500

    chore: added App.jsx that can toggle the two states

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,6 @@ import b from './entry_b.jsx';
 
 export default {
     a: import(/* webpackChunkName: 'a' */ './entry_a.jsx'),
+    // b: import(/* webpackChunkName: 'b' */ './entry_b.jsx'),
     b
 }
